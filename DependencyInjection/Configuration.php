90d293acdcd3d4cf4bcc236a57cdebb63c529372
Thomas Beaujean <thomas.beaujean@hotmail.fr>
2024-02-23 10:53:38 +0100

    fix interface compatibility

--- DependencyInjection/Configuration.php
+++ DependencyInjection/Configuration.php
@@ -10,7 +10,7 @@ class Configuration implements ConfigurationInterface
     /**
      * @return TreeBuilder
      */
-    public function getConfigTreeBuilder()
+    public function getConfigTreeBuilder(): TreeBuilder
     {
         $treeBuilder = new TreeBuilder('boilerplate');
         $rootNode = $treeBuilder->getRootNode();
